64af7a8c4ef5862f5a93a1badda899f239787dd2
pakin.ming <pakin.mi@bop.co.th>
2024-01-26 15:53:41 +0700

    add service expense

--- src/main/java/com/expense/dto/exponse/UpdateExpenseDto.java
+++ src/main/java/com/expense/dto/exponse/UpdateExpenseDto.java
@@ -3,8 +3,6 @@ package com.expense.dto.exponse;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.OffsetDateTime;
-
 @Data
 @NoArgsConstructor
 public class UpdateExpenseDto {
--- src/main/java/com/expense/entity/ExpenseEntity.java
+++ src/main/java/com/expense/entity/ExpenseEntity.java
@@ -1,17 +1,7 @@
 package com.expense.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NonNull;
-import lombok.Value;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.OffsetDateTime;
 
